73b1fb31e16a62a02e6e6e6a5ecdd7c98bef31c6
Ginteight <linluc2008@gmail.com>
2025-10-19 19:43:05 +0200

    ajout docstring

--- Carte_et_PileInfos.py
+++ Carte_et_PileInfos.py
@@ -9,6 +9,8 @@ fenetre.title("Solitaire")
 fenetre.attributes('-topmost', 1)
 
 class Carte:
+    """ Représente une carte du jeu de Solitaire"""
+
     def __init__(self, couleur:str, valeur:int, visible: bool, pile: Pile, x:int = None, y:int = None) -> None:
         self.couleur: str = couleur
         self.valeur: int = valeur
@@ -17,12 +19,21 @@ class Carte:
         self.label = tk.Label(fenetre, borderwidth=0)
         self.x = x
         self.y = y
-        self.afficher_carte()
+        self.afficher_carte() # affiche la carte à sa création
 
     def donner_couleur_et_valeur(self) -> tuple:
+        """ 
+        Retourne la couleur et la valeur de la carte sous forme de tuple (couleur, valeur) 
+        donner_couleur_et_valeur()
+        >>> ('coeur', 5)
+        >>> ('pique', 12)
+        """
+        
         return (self.couleur, self.valeur)
     
     def afficher_carte(self) -> None:
+        """ Affiche la carte sur la fenêtre """
+
         if self.visible == True:
             self.img = PhotoImage(file="cartes/"+self.valeur+"_"+self.couleur+".gif")
         else:
@@ -35,7 +46,8 @@ class Carte:
         self.label.image = self.img 
     
     def deplacer_carte(self, x: int = None, y: int = None, carte_dessous = None) -> None:
-        """ Change la position de la carte sur la fenêtre """
+        """ Change la position de la carte sur la fenêtre (interface graphique)"""
+
         if x == None and y == None:
             return
         elif x == None:
@@ -57,6 +69,8 @@ class Carte:
             self.label.lift(carte_dessous.label) # permet de superposer les cartes correctement
 
     def changer_visibilite_image(self) -> None:
+        """ Change l'image de la carte en fonction de sa visibilité """
+
         if self.visible == True:
             self.img = PhotoImage(file="cartes/face_cachee.png")
             self.visible = False
@@ -69,6 +83,7 @@ class PileInfos(Pile):
     """ Permet de stocker des informations sur une Pile de cartes : 
     - le numéro de la Pile si c'est une Pile du plateau de jeu, None sinon
     - la couleur si c'est une Pile de fondation, None sinon
+    - la position x de la Pile sur la fenêtre
     """
     def __init__(self, numero: int, couleur:str, x: int) -> None:
         super().__init__()
